3c50cf27cfc2435a8f0f6ebfd95d212599c5c887
Guilherme <guidumasperes@gmail.com>
2020-11-22 22:02:52 -0300

    Finishing third project

--- BestRPGsever/app/src/main/java/com/example/bestrpgsever/GameActivity.java
+++ BestRPGsever/app/src/main/java/com/example/bestrpgsever/GameActivity.java
@@ -3,15 +3,41 @@ package com.example.bestrpgsever;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class GameActivity extends AppCompatActivity {
+    TextView gameNameTextView;
+    TextView developersTextView;
+    TextView releaseDateTextView;
+    TextView publishersTextView;
+    ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
 
+        gameNameTextView = (TextView) findViewById(R.id.gameNameTextView);
+        developersTextView = (TextView) findViewById(R.id.developersTextView);
+        releaseDateTextView = (TextView) findViewById(R.id.releaseDateTextView);
+        publishersTextView = (TextView) findViewById(R.id.publishersTextView);
+        imageView = (ImageView) findViewById(R.id.imageView);
+
+        //Game Name
         String gameName = (String) getIntent().getExtras().get("gameName");
-        System.out.println(gameName);
+        gameNameTextView.setText(gameName);
+        //Developers
+        String gameDevs = (String) getIntent().getExtras().get("gameDevs");
+        developersTextView.setText("Developers: " + gameDevs);
+        //Release Date
+        String gameRelDate = (String) getIntent().getExtras().get("gameRelDate");
+        releaseDateTextView.setText("Release Date: " + gameRelDate);
+        //Publishers
+        String gamePub = (String) getIntent().getExtras().get("gamePub");
+        publishersTextView.setText("Publisher: " + gamePub);
+        //Image
+        Integer gameImage = (Integer) getIntent().getExtras().get("gameImage");
+        imageView.setImageResource(gameImage);
     }
 }
--- BestRPGsever/app/src/main/java/com/example/bestrpgsever/MainActivity.java
+++ BestRPGsever/app/src/main/java/com/example/bestrpgsever/MainActivity.java
@@ -7,13 +7,32 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
 
+import java.util.Objects;
+
 public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
-    String[] rpgGames = new String[] {"Baldur's Gate", "Dragon Age: Origins", "Dragon's Dogma: Dark Arisen", "Gothic",
-                                      "Kingdoms of Amalur: Reckoning", "Risen", "The Elder Scrolls V: Skyrim",
-                                      "The Witcher 3: Wild Hunt", "Torchlight"
+    String[] rpgGames = new String[] {"Baldur's Gate: Enhanced Edition", "Dragon Age: Origins", "Dragon's Dogma: Dark Arisen", "Gothic",
+                                      "Kingdoms of Amalur: Reckoning", "The Elder Scrolls V: Skyrim",
+                                      "The Witcher 3: Wild Hunt"
                                      };
+    Integer[] rpgImages = new Integer[] {R.mipmap.baldurs_gate, R.mipmap.dragon_age_origins, R.mipmap.dragons_dogma,
+                                         R.mipmap.gothic, R.mipmap.kingdoms_of_amalur, R.mipmap.the_elder_scrolls,
+                                         R.mipmap.the_witcher_3
+                                        };
+    String[] rpgGamesDevs = new String[] {"Beamdog", "BioWare", "Capcom", "Piranha Bytes",
+                                          "Big Huge Games and 38 Studios", "Bethesda Game Studios",
+                                          "CD PROJEKT RED"
+                                         };
+    String[] rpgGamesRelDate = new String[] {"16/jan/2013", "6/nov/2009", "15/jan/2016", "15/mar/2001",
+                                             "7/feb/2012", "11/nov/2011",
+                                             "18/mai/2015"
+                                            };
+    String[] rpgGamesPublisher = new String[] {"Beamdog", "EA Games", "Capcom", "THQ Nordic",
+                                               "EA Games and 38 Studios", "Bethesda Softworks",
+                                               "CD PROJEKT RED"
+                                              };
     ListView listView;
 
     @Override
@@ -21,7 +40,10 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Link layout
         listView = (ListView) findViewById(R.id.listView);
+
+        //Make the adapter
         ArrayAdapter<String> rpgGamesAdapter = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_list_item_1, rpgGames); //Maybe change to list_item_2 later
         listView.setAdapter(rpgGamesAdapter);
         listView.setOnItemClickListener(this);
@@ -31,6 +53,10 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
         Intent moveToGameActivityIntent = new Intent(getBaseContext(), GameActivity.class);
         moveToGameActivityIntent.putExtra("gameName", rpgGames[i]);
+        moveToGameActivityIntent.putExtra("gameDevs", rpgGamesDevs[i]);
+        moveToGameActivityIntent.putExtra("gameRelDate", rpgGamesRelDate[i]);
+        moveToGameActivityIntent.putExtra("gamePub", rpgGamesPublisher[i]);
+        moveToGameActivityIntent.putExtra("gameImage", rpgImages[i]);
         startActivity(moveToGameActivityIntent);
     }
 }
